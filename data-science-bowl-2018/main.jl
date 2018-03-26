db8ae478d6dd85b099e2cbb239d77f8aa6895e2b
ylxdzsw <ylxdzsw@gmail.com>
2018-03-26 14:50:35 +0800

    switch

--- data-science-bowl-2018/main.jl
+++ data-science-bowl-2018/main.jl
@@ -141,6 +141,58 @@ function train_masker(N=5000)
     end
 end
 
+function predict()
+    fout = open("D:/data-science-bowl-2018/result/submission.csv")
+    fout << "ImageId,EncodedPixels\n"
+
+    for id in readdir(test_dir)
+        image = readdir("$test_dir/$id/images")[]
+        image = load("$test_dir/$id/images/$image") |> im2n
+
+        si = size(image) |> cdr
+        cover = zeros(u8, si)
+
+        levels = []
+        while all(x->x>=64, si)
+            mask_image = zeros(u8, (2, si...))
+            for mask in masks
+                box = get_box(mask)
+                maxsize = max(box[2] - box[1], box[4] - box[3])
+                maxsize > 16 && continue
+                if maxsize > 8
+                    for (x, y) in mask @when max(x-box[1]+1, box[2]-x, y-box[3]+1, box[4]-y) <= 8
+                        mask_image[1, x, y] = 1
+                    end
+                else
+                    for (x, y) in mask
+                        mask_image[2, x, y] = 1
+                    end
+                end
+            end
+
+            push!(levels, mask_image)
+            si = (si .+ 1) .÷ 2
+            masks = map(x->unique(map(x->(x.+1).÷2, x)), masks)
+        end
+
+        levels
+
+        while true
+            p = model.detect(image)
+            v, i = findmax(p)
+
+            sigmoid(v) < .4 && break
+            h, x, y = ind2sub(size(p), i)
+
+            if h == 1
+
+            else
+
+            end
+        end
+    end
+end
+
 function save_model()
     model.save("D:/data-science-bowl-2018/result/")
 end
